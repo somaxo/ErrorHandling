76ecb161c29018b2428d27eb5837d39c39bce613
somaxo <sylviaama7@gmail.com>
2024-06-19 14:23:28 +0100

    added more input fields and a loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import toast, { Toaster } from "react-hot-toast";
+import axios from "axios";
+import { FaRegEyeSlash } from "react-icons/fa";
 import "./App.css";
+import "./Components/Loader.jsx";
+import Loader from "./Components/Loader.jsx";
 
 function App() {
   const {
@@ -13,9 +16,12 @@ function App() {
   } = useForm();
 
   const [regError, setRegError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [show, setShow] = useState(false);
 
   const onSubmit = async (data) => {
     try {
+      setLoading(true);
       const res = await axios.post(
         "https://form-handling.onrender.com/api/v1/auth/register",
         data
@@ -28,6 +34,7 @@ function App() {
     } catch (error) {
       console.log(error);
       setRegError(error.response.data.message);
+      setLoading(false);
     }
   };
 
@@ -35,12 +42,17 @@ function App() {
 
   const adjustGap = errors ? "-mt-3" : "mt-6";
 
+  const toggleEye = () => {
+    setShow(!show);
+  };
+
+  const toggleShow = show ? "text" : "password";
+
   return (
-    <div className="bg-gray-200">
-      <Toaster position="top-center" />
+    <div className="bg-gray-200 h-full">
       <section>
         <div className=" relative items-center w-full px-5 py-12 mx-auto md:px-12 lg:px-20 max-w-7xl">
-          <div className=" bg-white w-full h-96 rounded-lg max-w-md mx-auto md:max-w-sm md:px-0 md:w-96 sm:px-4">
+          <div className=" bg-white w-full h-[580px] rounded-lg max-w-md mx-auto md:max-w-sm md:px-0 md:w-96 sm:px-4">
             <div className="flex flex-col">
               <div>
                 <h2 className="text-4xl text-black mt-5 mb-2">
@@ -53,16 +65,65 @@ function App() {
                 <p className={`text-red-500 font-semibold`}>{regError}</p>
               )}
 
-              {/* <input
-                value="https://jamstacker.studio/thankyou"
-                type="hidden"
-                name="_redirect"
-              /> */}
               <div className="mt-4 space-y-6">
                 <div className="col-span-full">
                   <label
+                    htmlFor="firstname"
+                    className="block -mb-2 text-sm font-medium text-gray-600 text-left pl-5 "
+                  >
+                    First name
+                  </label>
+                  <input
+                    {...register("firstname", {
+                      required: "firstname is required",
+                      pattern: {
+                        message: "Please fill in firstname",
+                      },
+                    })}
+                    id="firstname"
+                    type="text"
+                    placeholder="John"
+                    className={`block w-11/12 m-auto px-6 py-3 text-black bg-white border border-gray-200 rounded-full appearance-none placeholder:text-gray-400 focus:border-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm ${
+                      errors.firstname ? "border-red-500" : "border-black"
+                    } font-semibold mt-3 outline-0 `}
+                  />
+                  {errors.firstname && (
+                    <p className=" text-red-500 text-start text-[12px] w-11/12 mx-5 mt-1 h-2 ">
+                      {errors.firstname.message}
+                    </p>
+                  )}
+                </div>
+                <div className="col-span-full">
+                  <label
+                    htmlFor="last name"
+                    className="block -mb-2 text-sm font-medium text-gray-600 text-left pl-5 "
+                  >
+                    Lastname
+                  </label>
+                  <input
+                    {...register("lastname", {
+                      required: "lastname is required",
+                      pattern: {
+                        message: "Please fill in lastname",
+                      },
+                    })}
+                    id="lastname"
+                    type="text"
+                    placeholder="Doe"
+                    className={`block w-11/12 m-auto px-6 py-3 text-black bg-white border border-gray-200 rounded-full appearance-none placeholder:text-gray-400 focus:border-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm ${
+                      errors.lastname ? "border-red-500" : "border-black"
+                    } font-semibold mt-3 outline-0 `}
+                  />
+                  {errors.lastname && (
+                    <p className=" text-red-500 text-start text-[12px] w-11/12 mx-5 mt-1 h-2 ">
+                      {errors.lastname.message}
+                    </p>
+                  )}
+                </div>
+                <div className="col-span-full relative">
+                  <label
                     htmlFor="password"
-                    className="block mb-3 text-sm font-medium text-gray-600 text-left pl-5 "
+                    className="block -mb-2 text-sm font-medium text-gray-600 text-left pl-5 "
                   >
                     Password
                   </label>
@@ -77,14 +138,19 @@ function App() {
                       },
                     })}
                     id="password"
-                    type="password"
+                    type={`${toggleShow}`}
                     placeholder="******"
                     className={`block w-11/12 m-auto px-6 py-3 text-black bg-white border border-gray-200 rounded-full appearance-none placeholder:text-gray-400 focus:border-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm ${
                       errors.password ? "border-red-500" : "border-black"
                     } font-semibold mt-3 outline-0 `}
                   />
+
+                    onClick={toggleEye}
+                    className="absolute right-10 top-10 "
+                  />
+
                   {errors.password && (
-                    <p className=" text-red-500 text-start text-[10px] w-11/12 mx-5 mt-1 h-2 ">
+                    <p className=" text-red-500 text-start text-[12px] w-11/12 mx-5 mt-1 h-2 ">
                       {errors.password.message}
                     </p>
                   )}
@@ -92,14 +158,14 @@ function App() {
                 <div className="col-span-full">
                   <label
                     htmlFor="confirmPassword"
-                    className="block mb-3 text-sm font-medium text-gray-600 text-left pl-5"
+                    className="block mb-1 text-sm font-medium text-gray-600 text-left pl-5"
                   >
                     Confirm password
                   </label>
                   <input
                     {...register("confirmPassword", {
                       required: " Confirm your password",
-  
+
                       validate: (value) =>
                         value === password || "Passwords do not match",
                     })}
@@ -108,8 +174,9 @@ function App() {
                     placeholder="******"
                     className="block w-11/12 m-auto px-6 py-3 text-black bg-white border border-gray-200 rounded-full appearance-none placeholder:text-gray-400 focus:border-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm"
                   />
+
                   {errors.confirmPassword && (
-                    <p className="text-red-500 text-start text-[10px] w-11/12 mx-5 mt-1 h-1">
+                    <p className="text-red-500 text-start text-[12px] w-11/12 mx-5 mt-1 h-1">
                       {errors.confirmPassword.message}
                     </p>
                   )}
@@ -118,9 +185,9 @@ function App() {
                 <div className="col-span-full">
                   <button
                     type="submit"
-                    className="items-center justify-center w-11/12 m-auto px-6 py-2.5 text-center text-white duration-200 bg-black border-2 border-black rounded-full nline-flex hover:bg-transparent hover:border-black hover:text-black focus:outline-none focus-visible:outline-black text-sm focus-visible:ring-black mt-3"
+                    className="items-center justify-center w-11/12 m-auto px-6 py-2.5 text-center text-black duration-200 bg-blue-300 border-2 border-blue-300 rounded-full nline-flex hover:bg-transparent hover:border-blue-300 hover:text-black focus:outline-none focus-visible:outline-blue-300 text-sm focus-visible:ring-black mt-3"
                   >
-                    Submit your request
+                    {loading ? <Loader /> : "Submit your request"}
                   </button>
                 </div>
               </div>
---
+++ src/Components/Loader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../styles/loader.css";
+
+const Loader = () => {
+  return (
+    <div>
+      <div class="loader">
+        {/* <span class="loader-text">loading</span> */}
+        <span class="load"></span>
+      </div>
+    </div>
+  );
+}
+
+export default Loader
